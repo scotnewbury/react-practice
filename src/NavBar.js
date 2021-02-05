77621adc20ae1a8a94bc00c50201df014645cdcb
Scot Newbury <scot@scotnewbury.com>
2021-02-05 12:05:25 -0500

    initial add for navbar styling

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import './NavBar.js';
 
 const NavBar = () => (
-  <nav>
+  <div class="navbar">
     <ul>
       <li><Link to="/">Home</Link></li>
       <li><Link to="/about">About</Link></li>
       <li><Link to="/projects">Projects</Link></li>
     </ul>
-  </nav>
+  </div>
 );
 
 export default NavBar;
