50cff13e95e293d368270f90a91829b95a003a4f
Scot Newbury <scot@scotnewbury.com>
2021-02-11 12:29:00 -0500

    updated style sheets

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+import './Navbar.css'
+
+const Navbar = () => {
+  
+  const [navLinkOpen, navLinkToggle] = useState(false);
+
+  const handleNavLinksToggle = () => {
+    navLinkToggle(!navLinkOpen);
+  };
+
+  const renderClasses = () => {
+    let classes  = "navlinks";
+
+    if (navLinkOpen) {
+      classes += " active"
+    }
+
+    return classes
+  }
+
+  return (
+   <navbar>
+      <div className="logo">
+        <i className='fa fa-laptop'></i>
+        <h4>Scot Newbury</h4>
+      </div>
+      <ul className={renderClasses()}>
+        <li className="link"><Link to="/">Home</Link></li>
+        <li className="link"><Link to="/about">About</Link></li>
+        <li className="link"><Link to="/projects">Projects</Link></li>
+        <li className="link"><Link to="/contactme">Contact Me</Link></li>
+      </ul>
+      <div onClick={handleNavLinksToggle} className="hamburger-toggle">
+        <i className="fas fa-bars fa-lg"></i>
+      </div>
+  </navbar>
+  )
+}
+
+export default Navbar;
--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -4,12 +4,9 @@ import bgVideo from '../assets/video/mountain-and-fog.mp4';
 
 const VideoBackground = () => {
   return (
-    <>
-    <div>Just a test line,</div>
-    <video autoPlay loop muted id='video'>
+    <video autoPlay loop muted className='video'>
       <source src={bgVideo} type='video/mp4' />   
     </video>
-    </>
   )
 }
 
