a18b1a4ac456cb97dec72e3bfe11843c9f3c2ea3
Scot Newbury <scot@scotnewbury.com>
2021-02-12 11:22:11 -0500

    migrated to jsx format

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPage = () => (
+  <div className="pagestyle">
+    <div className="showcase">
+      <h1>About Page</h1>
+    </div>
+  </div>
+);
+
+export default AboutPage;
